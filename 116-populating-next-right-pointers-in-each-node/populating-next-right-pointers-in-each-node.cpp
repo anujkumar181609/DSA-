b453e38eec940ad15a5836514bcb06e43c846036
Anuj Kumar <anujkumar181609@gmail.com>
2026-08-30 09:33:12 +0530

    Time: 13 ms (58.33%) | Memory: 20 MB (5.16%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,52 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==NULL) return root;
+        
+        unordered_map<int,vector<Node*>>m;
+        queue<Node*>q;
+        q.push(root);
+        
+        int level=-1;
+        while(!q.empty()){
+            int n=q.size();
+            level++;
+
+            while(n--){
+                Node* temp= q.front();
+                q.pop();
+
+                m[level].push_back(temp);
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+        }
+
+        for(int i=0;i<m.size();i++){
+            for(int j=1;j<m[i].size();j++) m[i][j-1]->next= m[i][j];
+
+            int n=m[i].size();
+            m[i][n-1]=NULL;
+        }
+        return root;
+        
+    }
+};
