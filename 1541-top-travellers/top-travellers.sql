7f11a5d1a4f23b05e62858694f848ffbdf5b0e5d
Anuj Kumar <anujkumar181609@gmail.com>
2026-09-21 18:40:10 +0530

    Time: 853 ms (83.13%) | Memory: 0B (100.00%) - LeetSync

--- 1541-top-travellers/top-travellers.sql
+++ 1541-top-travellers/top-travellers.sql
@@ -2,5 +2,5 @@
 SELECT Users.name, COALESCE(SUM(Rides.distance),0) AS travelled_distance FROM Users
 LEFT JOIN Rides
 ON Users.id= Rides.user_id
-GROUP BY Users.name, Users.id
+GROUP BY Users.id
 ORDER BY COALESCE(SUM(Rides.distance),0) DESC, Users.name ASC;
